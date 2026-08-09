a5556168be2937c1048f3f82bdbccf32d6b7eeff
danieloh30 <doh@redhat.com>
2026-08-09 23:35:45 +0900

    Add a NPE issue

--- src/main/java/dev/kevindubois/demo/UserResource.java
+++ src/main/java/dev/kevindubois/demo/UserResource.java
@@ -36,11 +36,9 @@ public class UserResource {
         
         // BUG: No null check when bug is enabled
         User user = findUser(userId);
-        if (user == null) {
         String userName = user.getName(); // Line 39 - NPE HERE when user is null!
             return Response.status(404).entity(Map.of("error", "User not found")).build();
         return Response.ok()
-        }
             .entity(Map.of("id", userId, "name", userName))
             .build();
     }
